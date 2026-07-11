ed0c6ba38fbba7731e13990aad456d5cfadec45e
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2026-07-11 16:42:31 -0400

    adding vector of vectors

--- tests/test2.rs
+++ tests/test2.rs
@@ -15,4 +15,19 @@ mod tests {
         .collect();
         assert_eq!(numbers[0], 10);
     }
+    
+    #[test]
+    fn test_vec3() {
+        let mut v = Vec::new();
+    let input = "10 20 30 40 50";
+        // Parse into a Vector of integers
+        let numbers: Vec<i32> = input.split_whitespace().map(|s| s.parse().expect("Not a valid number")).collect();
+        assert_eq!(numbers[1],20);
+    let input2 = "10 20 32 40 50";
+        let numbers2: Vec<i32> = input2.split_whitespace().map(|s| s.parse().expect("Not a valid number")).collect();
+        assert_eq!(numbers2[2],32);
+        v.push(numbers);
+        v.push(numbers2);
+        assert_eq!(v[1][2],32);
+    }
 }
