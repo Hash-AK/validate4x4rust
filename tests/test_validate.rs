aa456e28aa3304e9ab5f6481d58d0f3f9632a1e7
Hash-AK <175879022+Hash-AK@users.noreply.github.com>
2026-08-08 15:06:15 -0400

    added validate_size

--- tests/test_validate.rs
+++ tests/test_validate.rs
@@ -4,8 +4,30 @@ mod tests {
     use super::*;
     use validate4x4rust::split;
     use validate4x4rust::addline;
+
+    fn validate_size(vector:Vec<Vec<i32>>) -> bool {
+        if vector.len() != 4{return false;}
+        for i in 0..3 {
+            if vector[i].len() != 4 {return false;}
+        }
+        return true;
+    }
+
     fn validate4x4(vector:Vec<Vec<i32>>) -> bool {
         return false;
+    }
+
+    fn test_setup() -> Vec<Vec<i32>> {
+        let mut v = Vec::new();
+        let line1 = "1 2 3 4";
+        addline(line1.to_string(), &mut v);
+        let line2 = "3 4 1 2";
+        addline(line2.to_string(), &mut v);
+        let line3 = "2 1 4 3";
+        addline(line3.to_string(), &mut v);
+        let line4 = "4 3 2 1";
+        addline(line4.to_string(), &mut v);
+        return v;
     }
 
     #[test]
@@ -18,16 +40,13 @@ mod tests {
     }
     
     #[test]
-    fn test_validate() {
-        let mut v = Vec::new();
-        let line1 = "1 2 3 4";
-        addline(line1.to_string(), &mut v);
-        let line2 = "3 4 1 2";
-        addline(line2.to_string(), &mut v);
-        let line3 = "2 1 4 3";
-        addline(line3.to_string(), &mut v);
-        let line4 = "4 3 2 1";
-        addline(line4.to_string(), &mut v);
+    fn test_validate_size() {
+        let mut v = test_setup();
+        assert!(validate_size(v));
+    }
+    #[test]
+    fn test_validate4x4() {
+        let mut v = test_setup();
         assert!(validate4x4(v));
     }
 }
